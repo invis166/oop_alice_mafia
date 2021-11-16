986acbf1bc30065707734dfee40dfad4b48235af
amagoose <amagooseoj@mail.ru>
2021-11-17 03:44:22 +0500

    some comments

--- Domain&/Сourtesan.cs
+++ Domain&/Сourtesan.cs
@@ -3,7 +3,7 @@
     public class Сourtesan : IRole
     {
         public string Name => "Путана";
-
+        //потом поменяйте на givealibi я не могу я смеюсь
         public int FuckPlayer()
         {
             return 69;
