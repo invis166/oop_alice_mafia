84e6e0c58b2335568e68c826932f7a4114f5a464
invis166 <kolesnikovv028@gmail.com>
2021-11-29 02:21:49 +0500

    remove comment

--- Domain/Roles/IRole.cs
+++ Domain/Roles/IRole.cs
@@ -2,7 +2,6 @@
 {
     public interface IRole
     {
-        //i'm not sure вообще рефлексией по названиям классов можно наверное
         public string Name { get; }
 
         public void NightAction(IPlayer player);
