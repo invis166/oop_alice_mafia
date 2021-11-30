c7bef0ea1733be2d6b0b46172d5552a8ed205950
invis166 <kolesnikovv028@gmail.com>
2021-11-30 20:33:34 +0500

    change settings

--- Domain/Setting/DefaultSetting/DefaultGeneral.cs
+++ Domain/Setting/DefaultSetting/DefaultGeneral.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
+
 namespace AliceMafia.Setting.DefaultSetting
 {
     public class DefaultGeneral : IGeneralSetting
     {
         public string DayStartMessage { get; }
         public string DayEndMessage { get; }
+        public string GetKillMessage(IEnumerable<string> names)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- Domain/Setting/IGeneralSetting.cs
+++ Domain/Setting/IGeneralSetting.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
+
 namespace AliceMafia.Setting
 {
     public interface IGeneralSetting
     {
         public string DayStartMessage { get; }
         public string DayEndMessage { get; }
+        public string NightVotingMessage { get; }
+        public string DayVotingMessage { get; }
+        public string GetKillMessage(IEnumerable<string> names);
     }
 }
