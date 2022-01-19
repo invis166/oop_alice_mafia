d00f9ea992043fa2257e5568698435f3c738d2e2
alyona <anna.poluyanenko@gmail.com>
2022-01-20 02:37:38 +0500

    text correction

--- AliceMafia/Domain/Setting/DefaultSetting/DefaultCivilian.cs
+++ AliceMafia/Domain/Setting/DefaultSetting/DefaultCivilian.cs
@@ -3,6 +3,6 @@ namespace AliceMafia.Setting.DefaultSetting
     public class DefaultCivilian : IRoleSetting
     {
         public string Name => "Мирный житель";
-        public string NightActionMessage => "К сожалению, ночь. вы не можете ничего делать, так что вы просто мирно спите и надеетесь на лучшее.";
+        public string NightActionMessage => "К сожалению, вы не можете ничего делать, так что вы просто мирно спите и надеетесь на лучшее.";
     }
 }
